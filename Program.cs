bfe82bc3a8a4bd47c02f0c9c2fbdec9184e3e9b6
Kavish Singh <Kavish Singh@DESKTOP-S91V5DN>
2024-11-01 21:08:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            Console.WriteLine("Please enter your name : ");
+            Console.Write("");
+            username = Console.ReadLine();
+            Console.WriteLine("Greetings, " + username); 
+            Console.ReadLine();
+        }
+    }
+}
